4275948bb813dcee77080fa573591701b9d6c237
sandeepkumar <sandeepkumar.bala@aspiresys.com>
2024-06-14 16:45:08 +0530

    SetUser

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { getAccessToken } from "./Auth";
 
 const UserInfo = () => {
-    const [user] = useState(null);
+    const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
